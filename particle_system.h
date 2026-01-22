fd7f2ee0f4fe00f51afd5f947a7723e6a658a164
TaigaMatsuokaMizuno <taiga.matsuoka01@estudiant.upf.edu>
2026-01-22 18:07:12 +0100

    particle1

---
+++ particle_system.h
@@ -0,0 +1,20 @@
+//
+//  partile_system.h
+//  ComputerGraphics
+//
+//  Created by Taiga Matsuoka on 22/1/26.
+//
+
+#pragma once
+
+#include "main/includes.h"
+#include "framework.h"
+
+
+class Image;
+
+class ParticleSystem
+{
+    ...
+    void Render(Image* framebuffer);
+};
