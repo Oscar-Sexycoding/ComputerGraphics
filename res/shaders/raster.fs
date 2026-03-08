2bead68d1de7e967ae2e6b923613e182bc9c72ca
TaigaMatsuokaMizuno <taiga.matsuoka01@estudiant.upf.edu>
2026-03-08 06:12:22 +0100

    start 2.5

---
+++ res/shaders/raster.fs
@@ -0,0 +1,10 @@
+// This variables comes from the vertex shader
+// They are baricentric interpolated by pixel according to the distance to every vertex
+varying vec2 v_uv;
+uniform sampler2D u_texture;
+void main()
+{
+	// Set the ouput color per pixel
+    vec4 color = texture2D(u_texture, v_uv);
+    gl_FragColor = color;
+}
